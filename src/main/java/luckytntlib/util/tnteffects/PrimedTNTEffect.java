4a1fd794ffddcbce7e48b9e667ba5be3a96a32ef
Fleshcrafter <geissler.mael@gmail.com>
2024-05-22 11:47:13 +0200

    50.0.20.0

--- src/main/java/luckytntlib/util/tnteffects/PrimedTNTEffect.java
+++ src/main/java/luckytntlib/util/tnteffects/PrimedTNTEffect.java
@@ -175,7 +175,7 @@ public abstract class PrimedTNTEffect{
 	 * @return {@link Item} used for the {@link ItemStack} of {@link PrimedTNTEffect#getItemStack()}
 	 */
 	public Item getItem() {
-		return Items.AIR;
+		return Items.CARROT;
 	}
 	
 	/**
